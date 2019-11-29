69aa1acc5f946ca6487967efbfdfcb303f569344
Zack <zack@ukaton.com>
2019-11-28 20:42:12 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pink-trombone-editor
+ 
---
+++ pitch-detector/sketch.js
@@ -0,0 +1,49 @@
+
+
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Basic Pitch Detection
+=== */
+
+let audioContext;
+let mic;
+let pitch;
+
+async function setup() {
+  audioContext = new AudioContext();
+  window.audioContext = audioContext;
+  window.addEventListener("click", event => {
+    if(audioContext.state !== "running")
+      audioContext.resume();
+  });
+  stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+  startPitch(stream, audioContext);
+}
+setup();
+
+function startPitch(stream, audioContext) {
+  pitch = ml5.pitchDetection('./model/', audioContext , stream, modelLoaded);
+}
+
+function modelLoaded() {
+  document.querySelector('#status').textContent='Model Loaded';
+  getPitch();
+}
+
+function getPitch() {
+  pitch.getPitch(function(err, frequency) {
+    window.getPitchCallback(...arguments);
+
+    if (frequency) {
+      document.querySelector('#result').textContent = frequency;
+    } else {
+      document.querySelector('#result').textContent = 'No pitch detected';
+    }
+    getPitch();
+  })
+}
